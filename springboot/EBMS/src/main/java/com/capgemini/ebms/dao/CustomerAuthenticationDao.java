4a88a65296597f917f05c9679f4a6c91fa1e1fbb
Vikash <yequalstovikash@gmail.com>
2020-05-27 21:28:43 +0530

    Revert "day 1"
    This reverts commit fc15de8f04cba30f081af25e3d837ae8297ee608.

--- springboot/EBMS/src/main/java/com/capgemini/ebms/dao/CustomerAuthenticationDao.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.ebms.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capgemini.ebms.beans.CustomerAuthentication;
-
-public interface CustomerAuthenticationDao extends JpaRepository<CustomerAuthentication,Long>{
-
-}
