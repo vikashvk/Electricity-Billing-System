1bdef1664f5c6b902ddd577c23c38a3b0c77f797
Vikash <yequalstovikash@gmail.com>
2020-06-11 16:32:11 +0530

    payment

--- ebs_backend_springboot/src/main/java/com/ebs/config/SecurityConfig.java
+++ ebs_backend_springboot/src/main/java/com/ebs/config/SecurityConfig.java
@@ -79,7 +79,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf()
 				.disable().formLogin().disable().httpBasic().disable().exceptionHandling()
 				.authenticationEntryPoint(new RestAuthenticationEntryPoint()).and().authorizeRequests()
+<<<<<<< Updated upstream
 				.antMatchers("/", "/error").permitAll().antMatchers("/auth/**", "/oauth2/**").permitAll().anyRequest()
+=======
+				.antMatchers("/", "/error").permitAll().antMatchers("/auth/**","/payment/**", "/oauth2/**","/api/**").permitAll().anyRequest()
+>>>>>>> Stashed changes
 				.authenticated().and().oauth2Login().authorizationEndpoint().baseUri("/oauth2/authorize")
 				.authorizationRequestRepository(cookieAuthorizationRequestRepository()).and().redirectionEndpoint()
 				.baseUri("/oauth2/callback/*").and().userInfoEndpoint().userService(customOAuth2UserService).and()
